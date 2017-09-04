f94ae1e24b6f21afb280395a3f7f62c2eec4c207
siuming <2457944792@qq.com>
2017-09-04 21:01:18 +0800

    整合重构

--- RasterRender/Const/PrimitiveConst.cs
+++ RasterRender/Const/PrimitiveConst.cs
@@ -10,50 +10,50 @@ namespace RasterRender.Const
     {
         public static List<Vertex> CubeVertexs = new List<Vertex>()
         {
-            new Vertex() {pos = new Vector3(1, 1, 1), uv = new Vector2(1, 1)},
-            new Vertex() {pos = new Vector3(1, 1, 1), uv = new Vector2(1, 0)},
-            new Vertex() {pos = new Vector3(1, 1, 1), uv = new Vector2(0, 1)},
+            new Vertex() {pos = new Vector3(1, 1, 1), uv = new TexCoord(1, 1)},
+            new Vertex() {pos = new Vector3(1, 1, 1), uv = new TexCoord(1, 0)},
+            new Vertex() {pos = new Vector3(1, 1, 1), uv = new TexCoord(0, 1)},
             //3
-            new Vertex() {pos = new Vector3(-1, 1, 1), uv = new Vector2(0, 1)},
-            new Vertex() {pos = new Vector3(-1, 1, 1), uv = new Vector2(0, 0)},
-            new Vertex() {pos = new Vector3(-1, 1, 1), uv = new Vector2(1, 1)},
+            new Vertex() {pos = new Vector3(-1, 1, 1), uv = new TexCoord(0, 1)},
+            new Vertex() {pos = new Vector3(-1, 1, 1), uv = new TexCoord(0, 0)},
+            new Vertex() {pos = new Vector3(-1, 1, 1), uv = new TexCoord(1, 1)},
             //6
-            new Vertex() {pos = new Vector3(1, -1, 1), uv = new Vector2(1, 0)},
-            new Vertex() {pos = new Vector3(1, -1, 1), uv = new Vector2(0, 0)},
-            new Vertex() {pos = new Vector3(1, -1, 1), uv = new Vector2(1, 1)},
+            new Vertex() {pos = new Vector3(1, -1, 1), uv = new TexCoord(1, 0)},
+            new Vertex() {pos = new Vector3(1, -1, 1), uv = new TexCoord(0, 0)},
+            new Vertex() {pos = new Vector3(1, -1, 1), uv = new TexCoord(1, 1)},
             //9
-            new Vertex() {pos = new Vector3(-1, -1, 1), uv = new Vector2(0, 0)},
-            new Vertex() {pos = new Vector3(-1, -1, 1), uv = new Vector2(1, 0)},
-            new Vertex() {pos = new Vector3(-1, -1, 1), uv = new Vector2(0, 1)},
+            new Vertex() {pos = new Vector3(-1, -1, 1), uv = new TexCoord(0, 0)},
+            new Vertex() {pos = new Vector3(-1, -1, 1), uv = new TexCoord(1, 0)},
+            new Vertex() {pos = new Vector3(-1, -1, 1), uv = new TexCoord(0, 1)},
             //12
-            new Vertex() {pos = new Vector3(1, 1, -1), uv = new Vector2(1, 1)},
-            new Vertex() {pos = new Vector3(1, 1, -1), uv = new Vector2(1, 1)},
-            new Vertex() {pos = new Vector3(1, 1, -1), uv = new Vector2(0, 1)},
+            new Vertex() {pos = new Vector3(1, 1, -1), uv = new TexCoord(1, 1)},
+            new Vertex() {pos = new Vector3(1, 1, -1), uv = new TexCoord(1, 1)},
+            new Vertex() {pos = new Vector3(1, 1, -1), uv = new TexCoord(0, 1)},
             //15
-            new Vertex() {pos = new Vector3(-1, 1, -1), uv = new Vector2(0, 1)},
-            new Vertex() {pos = new Vector3(-1, 1, -1), uv = new Vector2(0, 1)},
-            new Vertex() {pos = new Vector3(-1, 1, -1), uv = new Vector2(0, 0)},
+            new Vertex() {pos = new Vector3(-1, 1, -1), uv = new TexCoord(0, 1)},
+            new Vertex() {pos = new Vector3(-1, 1, -1), uv = new TexCoord(0, 1)},
+            new Vertex() {pos = new Vector3(-1, 1, -1), uv = new TexCoord(0, 0)},
             //18
-            new Vertex() {pos = new Vector3(1, -1, -1), uv = new Vector2(1, 0)},
-            new Vertex() {pos = new Vector3(1, -1, -1), uv = new Vector2(0, 0)},
-            new Vertex() {pos = new Vector3(1, -1, -1), uv = new Vector2(1, 1)},
+            new Vertex() {pos = new Vector3(1, -1, -1), uv = new TexCoord(1, 0)},
+            new Vertex() {pos = new Vector3(1, -1, -1), uv = new TexCoord(0, 0)},
+            new Vertex() {pos = new Vector3(1, -1, -1), uv = new TexCoord(1, 1)},
             //21
-            new Vertex() {pos = new Vector3(-1, -1, -1), uv = new Vector2(0, 0)},
-            new Vertex() {pos = new Vector3(-1, -1, -1), uv = new Vector2(1, 0)},
-            new Vertex() {pos = new Vector3(-1, -1, -1), uv = new Vector2(0, 1)},
+            new Vertex() {pos = new Vector3(-1, -1, -1), uv = new TexCoord(0, 0)},
+            new Vertex() {pos = new Vector3(-1, -1, -1), uv = new TexCoord(1, 0)},
+            new Vertex() {pos = new Vector3(-1, -1, -1), uv = new TexCoord(0, 1)},
 
         };
 
         public static Vertex[] mesh = new Vertex[]
         {
-            new Vertex() {pos = new Vector4(1, -1, 1),   uv = new Vector2(1, 1), color = new Color(1.0f, 0.2f, 0.2f), rhw = 1},
-            new Vertex() {pos = new Vector4(-1, -1, 1),  uv = new Vector2(0, 1), color = new Color(0.2f, 1.0f, 0.2f), rhw = 1},
-            new Vertex() {pos = new Vector4(-1, 1, 1),   uv = new Vector2(1, 0), color = new Color(0.2f, 0.2f, 1.0f), rhw = 1},
-            new Vertex() {pos = new Vector4(1, 1, 1),    uv = new Vector2(0, 0), color = new Color(1.0f, 0.2f, 1.0f), rhw = 1},
-            new Vertex() {pos = new Vector4(1, -1, -1),  uv = new Vector2(1, 1), color = new Color(1.0f, 0.2f, 0.2f), rhw = 1},
-            new Vertex() {pos = new Vector4(-1, -1, -1), uv = new Vector2(0, 1), color = new Color(0.2f, 1.0f, 1.0f), rhw = 1},
-            new Vertex() {pos = new Vector4(-1, 1, -1),  uv = new Vector2(1, 0), color = new Color(1.0f, 0.3f, 0.3f), rhw = 1},
-            new Vertex() {pos = new Vector4(1, 1, -1),   uv = new Vector2(0, 0), color = new Color(0.2f, 1.0f, 0.3f), rhw = 1},
+            new Vertex() {pos = new Vector4(1, -1, 1),   uv = new TexCoord(1, 1), color = new Color(1.0f, 0.2f, 0.2f), rhw = 1},
+            new Vertex() {pos = new Vector4(-1, -1, 1),  uv = new TexCoord(0, 1), color = new Color(0.2f, 1.0f, 0.2f), rhw = 1},
+            new Vertex() {pos = new Vector4(-1, 1, 1),   uv = new TexCoord(1, 0), color = new Color(0.2f, 0.2f, 1.0f), rhw = 1},
+            new Vertex() {pos = new Vector4(1, 1, 1),    uv = new TexCoord(0, 0), color = new Color(1.0f, 0.2f, 1.0f), rhw = 1},
+            new Vertex() {pos = new Vector4(1, -1, -1),  uv = new TexCoord(1, 1), color = new Color(1.0f, 0.2f, 0.2f), rhw = 1},
+            new Vertex() {pos = new Vector4(-1, -1, -1), uv = new TexCoord(0, 1), color = new Color(0.2f, 1.0f, 1.0f), rhw = 1},
+            new Vertex() {pos = new Vector4(-1, 1, -1),  uv = new TexCoord(1, 0), color = new Color(1.0f, 0.3f, 0.3f), rhw = 1},
+            new Vertex() {pos = new Vector4(1, 1, -1),   uv = new TexCoord(0, 0), color = new Color(0.2f, 1.0f, 0.3f), rhw = 1},
         };
 
 }
